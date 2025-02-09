8aa417b9da383badce967c0e1a8c2343c5290114
Muhammed  Ashfaq <ashfaqmohammedcv@gmail.com>
2025-02-09 19:57:29 +0530

    updated

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 3004;
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/get_user_details',(req,res) => {
-    res.send({
-        user:{
-            name:"satyam",
-            age:22,
-            contact:12254
-        }
-    })
+app.get("/api/get_user_details", (req, res) => {
+  res.send({
+    user: {
+      name: "satyam",
+      age: 22,
+      contact: 12254,
+    },
+  });
 });
 
 app.listen(PORT, () => {
