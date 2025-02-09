999fdb7073c85c647ed281796db56e69907a61fc
Muhammed  Ashfaq <ashfaqmohammedcv@gmail.com>
2025-02-09 17:19:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
